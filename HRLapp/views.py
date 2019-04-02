c8ac8cda03e692b1d8115085b966be27c6101a77
Raaabbit <yangflo@foxmail.com>
2019-04-02 15:33:08 +0800

    实现导出文件功能

--- HRLapp/views.py
+++ HRLapp/views.py
@@ -8,16 +8,24 @@ from refs.utils.interface import getBP
 def index(req):
     return render(req,"index.html")
 
+# 文件写入函数
 def writeFile(fileData,path):
-    with open(path,"wb+") as stfk:
-        for chunk in fileData.chunks():
-            stfk.write(chunk)
+    with open(path,"wb+") as f:
+        if(fileData.chunks):
+            for chunk in fileData.chunks():
+                f.write(chunk)
+        else:
+            f.write(fileData)
 
+# 生成文件并将路径传入getBP以生成黑点信息
 def upload(req):
     roadFiles = req.FILES["roadfile"]
     timeFiles = req.FILES["timefile"]
     writeFile(roadFiles,'static/road.csv')
     writeFile(timeFiles,'static/time.csv')
     resJson = getBP('static/time.csv','static/road.csv')
+    with open('static/res.json','w') as f:
+        # json.dump(resJson,f)
+        f.write(resJson)
     return HttpResponse(json.dumps(resJson),content_type="application/json")
 
--- static/JS/main.js
+++ static/JS/main.js
@@ -3,10 +3,10 @@ window.onload = function(){
     var formData = new FormData();
     $('#roadFile').on('change',function (event) {
         $('#road-file-name').text(event.target.files[0].name);
-    })
+    });
     $('#timeFile').on('change',function (event) {
         $('#time-file-name').text(event.target.files[0].name);
-    })
+    });
     // 上传
     $('#submit-btn').on('click',function(event){
         event.preventDefault();
@@ -25,11 +25,12 @@ window.onload = function(){
         }).done((res)=>{
             // 得到分析结果，生成地图
             var resJson = JSON.parse(res);
+            // 在 sessionStorage 缓存分析结果
+            sessionStorage.setItem('hazardous-road-locations', JSON.stringify(resJson));
             // 百度地图API功能
             var map = new BMap.Map("main-map");
             var points = [];
             for (item in resJson){
-                console.log(item)
                 var point = new BMap.Point(resJson[item].bp_lon,resJson[item].bp_lat);
                 points.push(point);
             }
@@ -41,7 +42,23 @@ window.onload = function(){
             }
 
         }).fail((err)=>{
+            alert("分析失败，请检查上传文件是否正确");
             console.log(err);
         })
+    });
+
+    // 导出
+    $('#export-btn').on('click',function (event) {
+        event.preventDefault();
+        if(sessionStorage.getItem('hazardous-road-locations')){
+            var expData = new Blob([sessionStorage.getItem('hazardous-road-locations')],{type:'application/json'});
+            var aTag = document.createElement('a');
+            aTag.download = 'res.json';
+            aTag.href = URL.createObjectURL(expData);
+            aTag.click();
+            URL.revokeObjectURL(expData);
+        }else{
+            alert("请先正确进行分析");
+        }
     })
-}
+};
